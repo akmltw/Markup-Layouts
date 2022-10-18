a0f34aab083cbbf7146514659b827a9113dd763c
Akim <akm_ltw@mail.ru>
2022-10-18 21:28:40 +0300

    Loading local DigiDiga layot

---
+++ DigiDiga/DigiDiga/js/script.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper('.slider-recent-block', {
+   loop: true,
+   navigation: {
+      nextEl: 'recent-block__arrow.swiper-button-next',
+      prevEl: 'recent-block__arrow.swiper-button-prev',
+   },
+});
